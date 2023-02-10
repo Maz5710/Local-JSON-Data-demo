3890fb84c6105cdf9ee1f22916d41d8dd54ceb68
Maz5710 <marian.arnot@gmail.com>
2023-02-10 13:43:32 +1300

    correct index id s elect

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getAllStudents() {
 
         // Every Ajax needs a success and an error
 
-        success: function (studentData) { 
+        success: function (studentData) {
             document.getElementById('results').innerHTML = '';
             let i;
             for (i = 0; i < studentData.length; i++) {
@@ -44,7 +44,7 @@ function searchStudentsNames() {
         url: './students.json',
         type: 'GET',
         data: 'json',
-    
+
         success: function (studentData) {
             let results = document.getElementById('results');
             results.innerHTML = '';
@@ -71,7 +71,7 @@ function searchStudentsNames() {
                 }
             }
         },
-    
+
         error: function () {
             console.log('error - cannot filer');
         }
@@ -89,41 +89,40 @@ function dropDownSelect() {
         url: './students.json',
         type: 'GET',
         data: 'json',
-    
-        success: function (studentData) {
+
+        success: function (studentdata) {
             let results = document.getElementById('results');
             results.innerHTML = '';
-            let getSelection = document.getElementById('userSelection'),value;
-            for (let i = 0; i < studentData.length; i++) {
-                let student = studentData[i];
+            let getSelection = document.getElementById("userSelection").value;
+            for (let i = 0; i < studentdata.length; i++) {
+                let student = studentdata[i];
                 let firstName = student.first_name.toLowerCase();
                 let lastName = student.last_name.toLowerCase();
                 let checkSelection = getSelection.toLowerCase();
-                // Get the serach input value
-
+                //Get the search input value
                 if (checkSelection === firstName || checkSelection === lastName) {
-                results.innerHTML += `
-                <div class="col-md-4 col-12 mb-3">
-                <div class="card">
-                    <img src="${student.profile_img}" class="card-img-top" alt="Student Profile Image">
-                    <div class="card-body">
-                        <h5 class="card-title"><span class="first-name">${student.first_name}</span> <span class="last-name">${student.last_name}</span></h5>
-                        <p class="card-text">Id#: <span class="id-number">${student.id}</span></p>
-                        <p class="card-text">Email: <span class="email">${student.email}</span></p>
-                    </div>
-                </div>
-            </div>`
+                    results.innerHTML += `
+                    <div class="col-md-4 col-12 mb-3">
+                            <div class="card">
+                                <img src="${student.profile_img}" class="card-img-top" alt="Student Profile Image">
+                                <div class="card-body">
+                                    <h5 class="card-title"><span class="first-name">${student.first_name}</span> <span class="last-name">${student.last_name}</span></h5>
+                                    <p class="card-text">Id#: <span class="id-number">${student.id}</span></p>
+                                    <p class="card-text">Email: <span class="email">${student.email}</span></p>
+                                </div>
+                            </div>
+                        </div>
+                    `
                 }
             }
         },
-    
+
         error: function () {
             console.log('error - cannot find your selection');
         }
     })
 }
 
-$('#userSelection').on('ichange', function () {
+$('#userSelection').on('change', function () {
     dropDownSelect();
 })
-
